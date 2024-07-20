fe3ac3055c3f9ffb4cfeb464d77743238fef33f6
kemal sahin <kemalshnn267@gmail.com>
2024-07-20 21:10:16 +0300

    ders 5 bitti

---
+++ RealEstate_Dapper_Api/Dtos/CatageryDtos/UpdateCategoryDto.cs
@@ -0,0 +1,6 @@
+﻿namespace RealEstate_Dapper_Api.Dtos.CatageryDtos
+{
+    public class UpdateCategoryDto
+    {
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using RealEstate_Dapper_Api.Dtos.CatageryDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
+{
+    public interface ICategoryRepository
+    {
+        public Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+    }
+}
