31d93645af2e26a7e1fb37e5f6d34cebd3f0a211
kemal sahin <kemalshnn267@gmail.com>
2024-07-21 18:22:23 +0300

    ders 13 bitti

--- RealEstate_Dapper_UI/Program.cs
+++ RealEstate_Dapper_UI/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient(); // bunu projeleri haberkeþtirmek için yaptým
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
