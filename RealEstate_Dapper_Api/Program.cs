3cccb2dfe161ead76940854ddcc33cb4f280ba98
kemal sahin <kemalshnn267@gmail.com>
2024-07-23 14:52:16 +0300

    ders 19 bitti

--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -1,4 +1,5 @@
 using RealEstate_Dapper_Api.Models.DapperContext;
+using RealEstate_Dapper_Api.Repositories.BottomGridRepositories;
 using RealEstate_Dapper_Api.Repositories.CategoryRepository;
 using RealEstate_Dapper_Api.Repositories.ProductRepository;
 using RealEstate_Dapper_Api.Repositories.ServiceRepository;
@@ -15,6 +16,8 @@ builder.Services.AddTransient<IProductRepository,ProductRepository>();
 builder.Services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
 
 builder.Services.AddTransient<IServiceRepository, ServiceRepository>();
+
+builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>();
 
 
 
