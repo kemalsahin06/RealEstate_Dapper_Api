1b82843813fef87180925fb78209f4733f8d204b
kemal sahin <kemalshnn267@gmail.com>
2024-07-23 13:24:37 +0300

    ders 18 bitti

---
+++ RealEstate_Dapper_Api/Controllers/ServiceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.ServiceRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        public readonly IServiceRepository _serviceRepository;
+
+        public ServiceController(IServiceRepository serviceRepository)
+        {
+            _serviceRepository = serviceRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetServiceList()
+        {
+            var value =await _serviceRepository.GetAllServiceAsync();
+            return Ok(value);
+        }
+
+
+    }
+}
--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
@@ -16,17 +16,24 @@ namespace RealEstate_Dapper_UI.ViewComponents.HomePage
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:44346/api/WhoWeAreDetail");
+            var client2 = _httpClientFactory.CreateClient();
 
-            if (responseMessage.IsSuccessStatusCode)
+            var responseMessage = await client.GetAsync("https://localhost:44346/api/WhoWeAreDetail");
+            var responseMessage2 = await client2.GetAsync("https://localhost:44346/api/Service");
+
+            if (responseMessage.IsSuccessStatusCode && responseMessage2.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+
                 var value = JsonConvert.DeserializeObject<List<ResultWhoWeAreDetailDto>>(jsonData);
+                var value2 = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData2);
+
                 ViewBag.Title = value.Select(x=>x.Title).FirstOrDefault();
                 ViewBag.subTitle = value.Select(x=>x.SubTitle).FirstOrDefault();
                 ViewBag.desciription1 = value.Select(x=>x.Desciription1).FirstOrDefault();
                 ViewBag.desciription2 = value.Select(x=>x.Desciription2).FirstOrDefault();
-                return View();
+                return View(value2);
 
             }
             return View();
