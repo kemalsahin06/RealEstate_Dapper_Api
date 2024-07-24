4b1fe9759fdef9992da6b50dcaaf162051403981
kemal sahin <kemalshnn267@gmail.com>
2024-07-24 11:53:14 +0300

    ders 26 bitti

--- RealEstate_Dapper_Api/Controllers/CategoriesController.cs
+++ RealEstate_Dapper_Api/Controllers/CategoriesController.cs
@@ -31,7 +31,7 @@ namespace RealEstate_Dapper_Api.Controllers
             return Ok("Kategory başarılı bir şekilde eklendi");
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCategory(int id)
         {
             _categoryRepository.DeleteCategory(id);
